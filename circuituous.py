16e613f55c79b624565e9370b873309125674feb
George Kaimakis <geoptus@gmail.com>
2018-02-11 00:09:08 +0000

    Adds perimeter calculation method

--- circuituous.py
+++ circuituous.py
@@ -17,3 +17,7 @@ class Circle(object):
     def area(self):
         """Perform quadrature on a shape of uniform radius"""
         return math.pi * self.radius ** 2.0
+
+    def perimeter(self):
+        """Calculate the perimeter of a circle of radius R"""
+        return 2.0 * math.pi * self.radius
